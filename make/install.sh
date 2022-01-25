c50e4e3383eea1e3678babc8668fa301900c75a4
Cobolbaby <Zhang.Xing-Long@inventec.com>
2022-01-25 18:45:20 +0800

    fix: -f andon*.tar.gz check

--- make/install.sh
+++ make/install.sh
@@ -10,6 +10,8 @@ host=$1
 
 source common.sh
 
+set +o noglob
+
 item=1
 
 h2 "[Step $item]: checking if docker is installed ..."; let item+=1
@@ -36,8 +38,8 @@ docker-compose up -d zoo broker postgres
 
 # 等待 Kafka 启动之后，新建 Topic
 h2 "[Step $item]: creating kafka topics ...";  let item+=1
-docker exec -ti broker1 kafka-topics --create --bootstrap-server localhost:19092 --topic insight-andon-init --partitions 3
-docker exec -ti broker1 kafka-topics --create --bootstrap-server localhost:19092 --topic insight-andon-alarm --partitions 3
+docker exec -ti broker kafka-topics --create --bootstrap-server localhost:19092 --topic insight-andon-init --partitions 3
+docker exec -ti broker kafka-topics --create --bootstrap-server localhost:19092 --topic insight-andon-alarm --partitions 3
 
 h2 "[Step $item]: starting Andon ..."
 docker-compose up -d
